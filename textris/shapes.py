207772146ee38fa2a912402a475932af7d96df87
Matt Wheeler <m@funkyhat.org>
2019-02-07 20:23:18 +0000

    make a list of the shapes make a function to get a random shape

--- textris/shapes.py
+++ textris/shapes.py
@@ -1,10 +1,14 @@
+
+from random import choice
+
 import board
 
 
-def shape(coords):
+def shape(*coords):
     shape = board.Board((4, 4))
     for coord in coords:
         shape[coord] = 1
+    return shape
 
 
 line = shape((1, 0), (1, 1), (1, 2), (1, 3))
@@ -14,3 +18,10 @@ s2 = shape((0, 2), (1, 2), (1, 1), (2, 1))
 l1 = shape((0, 1), (0, 2), (1, 1), (2, 1))
 l2 = shape((0, 1), (0, 2), (1, 2), (2, 2))
 t = shape((0, 1), (1, 1), (2, 1), (1, 2))
+
+
+shapes = [line, square, s1, s2, l1, l2, t]
+
+
+def get_shape():
+    return choice(shapes)
